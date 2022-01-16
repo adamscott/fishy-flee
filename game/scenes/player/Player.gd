c68708521635edffbed96da769faf2953e5bf7cb
Adam Scott <ascott.ca@gmail.com>
2022-01-16 15:58:00 -0500

    Fix fish rotation

--- game/scenes/player/Player.gd
+++ game/scenes/player/Player.gd
@@ -5,10 +5,15 @@ const StateMachinePlayer: = YAFSM.StateMachinePlayer
 
 export (Vector3) var up_direction: = Vector3.UP
 export (float) var speed: = 20.0
-export (float) var min_velocity: = 0.3
+export (float) var min_velocity_horizontal: = 0.3
+export (float) var min_velocity_vertical: = 0.1
 export (float) var max_velocity_horizontal: = 5.0
-export (float) var max_velocity_vertical: = 2.0
-export (float) var friction: = 0.5
+export (float) var max_velocity_vertical: = 1.0
+export (float) var friction_horizontal: = 0.5
+export (float) var friction_vertical: = 1.0
+export (float) var rotation_speed_factor: = PI
+
+onready var skin: Spatial = $Skin
 
 onready var move_sm: StateMachinePlayer = $StateMachines/MoveStateMachine as StateMachinePlayer
 
@@ -82,6 +87,7 @@ func process_physics_before_gravity(delta: float) -> void:
 	apply_rotation(delta)
 	apply_movement(delta)
 	apply_friction(delta)
+	apply_rotation_redirection(delta)
 
 
 func process_physics_gravity(delta: float) -> void:
@@ -97,15 +103,21 @@ func process_physics_before_move_and_slide(delta: float) -> void:
 
 
 func process_physics_move_and_slide(delta: float) -> void:
-	if velocity.length_squared() < min_velocity * min_velocity:
-		velocity = Vector3.ZERO
+	var vertical_velocity = velocity * up_direction
+	var horizontal_velocity = velocity - vertical_velocity
+	
+	if horizontal_velocity.length_squared() < min_velocity_horizontal * min_velocity_horizontal:
+		horizontal_velocity = Vector3.ZERO
+	
+	if vertical_velocity.length_squared() < min_velocity_vertical * min_velocity_vertical:
+		vertical_velocity = Vector3.ZERO
+	
+	velocity = horizontal_velocity + vertical_velocity
 	
 	var integrated: = velocity + acceleration
 	
 	var integrated_vertical: = up_direction * integrated
 	var integrated_horizontal: = integrated - integrated_vertical
-
-	prints(integrated_horizontal, integrated_vertical)
 
 	if integrated_horizontal.length_squared() > max_velocity_horizontal * max_velocity_horizontal:
 		integrated_horizontal = integrated_horizontal.normalized() * max_velocity_horizontal
@@ -124,15 +136,45 @@ func process_physics_after_move_and_slide(delta: float) -> void:
 
 
 func apply_rotation(delta: float) -> void:
-	pass
+	if move_direction.is_equal_approx(Vector3.ZERO):
+		return
+
+	var target_direction: = global_transform.looking_at(
+		global_transform.origin + move_direction, 
+		up_direction
+	)
+	global_transform = global_transform.interpolate_with(target_direction, rotation_speed_factor * delta)
 
 
 func apply_movement(delta: float) -> void:
-	acceleration = move_direction * speed * delta
+	var vertical_direction: = move_direction * up_direction
+	var horizontal_direction: = move_direction - vertical_direction
+	acceleration = -global_transform.basis.z * horizontal_direction.length() * speed * delta
 
 
 func apply_friction(delta: float) -> void:
-	velocity = velocity.linear_interpolate(
+	var vertical_velocity: = velocity * up_direction
+	var horizontal_velocity: = velocity - vertical_velocity
+	
+	vertical_velocity = vertical_velocity.linear_interpolate(
 		Vector3.ZERO,
-		friction * delta
+		friction_vertical * delta
 	)
+	
+	horizontal_velocity = horizontal_velocity.linear_interpolate(
+		Vector3.ZERO,
+		friction_horizontal * delta
+	)
+	
+	velocity = horizontal_velocity + vertical_velocity
+
+
+func apply_rotation_redirection(delta: float) -> void:
+	var forward: = -global_transform.basis.z
+	
+	$Forward.global_transform = $Forward.global_transform.looking_at(
+		$Forward.global_transform.origin + velocity,
+		up_direction
+	)
+	
+	velocity = forward.normalized() * velocity.length()

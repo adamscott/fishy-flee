f1ad2208613e5e00fb48ddcb34acc029c611084f
Adam Scott <ascott.ca@gmail.com>
2022-01-23 11:44:47 -0500

    Add whip sounds to GWJ intro

--- game/scenes/scenery/algae_wall/AlgaeWall.gd
+++ game/scenes/scenery/algae_wall/AlgaeWall.gd
@@ -4,11 +4,13 @@ onready var wall: StaticBody = $Wall
 onready var disable_collision_area: Area = $DisableCollisionArea
 
 func _on_DisableCollisionArea_body_entered(body: Node) -> void:
-	wall.add_collision_exception_with(body)
+	if body is CollisionObject:
+		wall.add_collision_exception_with(body)
 
 
 func _on_DisableCollisionArea_body_exited(body: Node) -> void:
-	wall.remove_collision_exception_with(body)
+	if body is CollisionObject:
+		wall.remove_collision_exception_with(body)
 
 
 func _ready() -> void:
